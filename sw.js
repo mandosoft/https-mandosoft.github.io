85f386d3fb2ee84f0dc40661e728833e171839a9
mandosoft <thomasdtownsley@gmail.com>
2020-08-14 10:02:54 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1a7cad34dea6514cf22d.js"
+    "url": "webpack-runtime-82d1e842992db8b58cc1.js"
   },
   {
     "url": "framework-fd2b85530eec85104a39.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "54eefbd966f946642c98eabe8c0aab20"
+    "revision": "8af854b5fa2930341dccc645ae11df97"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
